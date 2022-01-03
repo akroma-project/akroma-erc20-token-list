135dbd3ff3916390167ecbe385d75c032cea5756
detroitpro <eric@polerecky.com>
2022-01-03 11:31:04 -0500

    chore: updated token list, updated model

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,42 @@
 export interface TokenInfo {
-  symbol: string;
-  name: string;
-  type: string;
   address: string;
   decimals: number;
+  ens_address: string;
+  logo: Logo;
+  name: string;
+  social: Social;
+  support: Support;
+  symbol: string;
+  type: string;
+  website: string;
+}
+
+export interface Logo {
+  height: string;
+  ipfs_hash: string;
+  src: string;
+  width: string;
+}
+
+export interface Social {
+  blog: string;
+  chat: string;
+  facebook: string;
+  forum: string;
+  github: string;
+  gitter: string;
+  instagram: string;
+  linkedin: string;
+  reddit: string;
+  slack: string;
+  telegram: string;
+  twitter: string;
+  youtube: string;
+}
+
+export interface Support {
+  email: string;
+  url: string;
 }
 
 /**
